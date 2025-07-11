336e01cc8ba29f4bd50de4215c3a5d3d95ee590d
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2025-07-11 07:25:17 +0330

    feat: implement sidebar component

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserButton from "@/features/auth/components/profile-menu-button";
-
-export default async function Home() {
-  return (
-    <div className="p-4">
-      <UserButton />
-    </div>
-  );
-}
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import { CircleCheck, Home, Settings, Users } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "My Tasks",
+    url: "",
+    icon: CircleCheck,
+  },
+  {
+    title: "Settings",
+    url: "",
+    icon: Settings,
+  },
+  {
+    title: "Members",
+    url: "",
+    icon: Users,
+  },
+];
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            {" "}
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      {item.title}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
